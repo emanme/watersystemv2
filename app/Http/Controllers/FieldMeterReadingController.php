c037c25828c5269397c518e729faa5d7532985cb
emanme <saligueeman@gmail.com>
2024-02-28 00:36:42 +0800

    change api endpoint

--- app/Http/Controllers/FieldMeterReadingController.php
+++ app/Http/Controllers/FieldMeterReadingController.php
@@ -11,6 +11,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
+use Illuminate\Http\JsonResponse;
+
 
 class FieldMeterReadingController extends Controller
 {
@@ -21,10 +23,7 @@ class FieldMeterReadingController extends Controller
         $this->waterbill = new WaterBillController();
     }
 
-    public function toAccounting($num)
-    {
-        return number_format($num, 2, '.', ',');
-    }
+    public function toAccounting($num){ return number_format($num, 2, '.', ','); }
 
     public function index()
     {
@@ -37,18 +36,22 @@ class FieldMeterReadingController extends Controller
         $index = 0;
         $customersLists = [];
 
-        if (isset($customers) || !empty($customers) || $customers != null) {
-            for ($i = 0; $i < $customers->count(); $i++) {
+        if(isset($customers) || !empty($customers) || $customers != null)
+        {
+            for($i = 0; $i < $customers->count(); $i++)
+            {
                 $balance = Transaction::orderByDesc('created_at')->where('customer_id', $customers[$i]->account())->get();
                 $balance = $balance->first();
 
-                if (isset($balance) || !empty($balance) || $balance != null) {
+                if(isset($balance) || !empty($balance) || $balance != null)
+                {
                     $monthNow = date('m');
                     $dayNow = date('d');
                     $prevMonth = date('t', strtotime($balance->reading_date));
                     $prevDay = date('d', strtotime($balance->reading_date));
 
-                    if ((($prevMonth - $prevDay) + $dayNow) >= 33) {
+                    if((($prevMonth - $prevDay) + $dayNow) >= 33)
+                    {
                         $customersLists[$index] = $customers[$i];
                         $index++;
                     }
@@ -71,35 +74,37 @@ class FieldMeterReadingController extends Controller
 
     public function filter(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(),[
             'barangay' => 'required',
             'purok' => 'required'
         ]);
 
-        if ($validator->fails()) {
+        if($validator->fails())
+        {
             return response()->json(['filtered' => false, 'errors' => $validator->errors()]);
         }
 
-        $data = Customer::where('purok', $request->purok)->where('barangay', $request->barangay)->get();
+        $data = Customer::where('purok' , $request->purok)->where('barangay' , $request->barangay)->get();
 
         return response()->json(['filtered' => true, 'data' => $data]);
     }
 
     public function search(Request $request)
     {
-        $account_number = $request->account_number;
+        $account_number=$request->account_number;
 
-        try {
-            $customer = Customer::findOrFail($account_number);
-        } catch (ModelNotFoundException $e) {
+        try{
+            $customer=Customer::findOrFail($account_number);
+        }catch(ModelNotFoundException $e){
             return back()->withErrors([
-                'account_number.exists' => 'Account number not found'
+                'account_number.exists'=>'Account number not found'
             ])->withInput();
         }
 
-        if (!$customer->hasActiveConnection()) {
+        if(!$customer->hasActiveConnection())
+        {
             return back()->withErrors([
-                'account_number' => 'Account is not active'
+                'account_number'=>'Account is not active'
             ])->withInput();
         }
 
@@ -117,8 +122,10 @@ class FieldMeterReadingController extends Controller
 
         $rates = WaterRate::all();
 
-        foreach ($rates as $r) {
-            if (Str::title($customer->connection_type) == $r->type) {
+        foreach($rates as $r)
+        {
+            if(Str::title($customer->connection_type) == $r->type)
+            {
                 $rate = [
                     'min_rate' => $r->min_rate,
                     'max_range' => $r->consumption_max_range,
@@ -130,13 +137,16 @@ class FieldMeterReadingController extends Controller
         $surcharge = Surcharge::all();
         $date = "";
         // $payments = Payments
-        if (count($balance->toArray()) > 0) {
-            $date = ($new_balance->period_covered != "Beginning Balance" ? explode('-', $new_balance->period_covered) : explode('/', '/' . $new_balance->reading_date));
-        } else {
-            $date = explode('/', '/' . date('Y-m-d'));
+        if(count($balance->toArray()) > 0)
+        {
+            $date = ($new_balance->period_covered != "Beginning Balance" ? explode('-', $new_balance->period_covered) : explode('/', '/'.$new_balance->reading_date));
+        }
+        else
+        {
+            $date = explode('/', '/'.date('Y-m-d'));
         }
 
-        return view('field-personnel.pages.meter-reading', [
+        return view('field-personnel.pages.meter-reading',[
             'customer' => [
                 'fullname' => $fullname,
                 'address' => $address,
@@ -145,7 +155,7 @@ class FieldMeterReadingController extends Controller
                 'meter_number' => $meter_sn,
                 'balance' => $new_balance,
                 'connection_type' => $customer->connection_type,
-                'org_name' => $customer->org_name,
+                'org_name'=>$customer->org_name,
                 'serial_number' => $customer->meter_number
             ],
             'rates' => $rate,
@@ -157,17 +167,17 @@ class FieldMeterReadingController extends Controller
 
     public function store(Request $request)
     {
-
-        if (isset($request->read_date)) {
-            if (
-                \Carbon\Carbon::parse($request->current_month) >= $request->read_date &&
-                \Carbon\Carbon::parse($request->next_month) <= $request->read_date
-            ) {
+        if(isset($request->read_date))
+        {
+            if( \Carbon\Carbon::parse($request->current_month) >= $request->read_date &&
+                \Carbon\Carbon::parse($request->next_month) <= $request->read_date )
+            {
                 return response()->json(['created' => false, 'msg' => 'Cannot create billing, make sure that the reading date is not covered from the previous reading date.']);
             }
         }
 
-        if ($request->reading_meter < $request->meter_reading) {
+        if($request->reading_meter < $request->meter_reading)
+        {
             return response()->json(['created' => false, 'msg' => 'Current meter reading should not be less than the previous meter reading.']);
         }
 
@@ -175,9 +185,9 @@ class FieldMeterReadingController extends Controller
         $this->waterbill->getCurrentBalance($request->customer_id);
         $this->waterbill->computeBillConsumption($request->reading_meter);
 
-        $fillable = [
+        $fillable=[
             'customer_id' => $this->waterbill->balance->customer_id,
-            'period_covered' => $request->current_month . '-' . $request->next_month,
+            'period_covered' => $request->current_month.'-'.$request->next_month,
             'reading_date' => date('Y-m-d', strtotime($request->read_date)),
             'reading_meter' => $request->reading_meter,
             'reading_consumption' => $this->waterbill->computed_total['meter_consumption'],
@@ -200,39 +210,104 @@ class FieldMeterReadingController extends Controller
 
 
         $transactions = Transaction::create($fillable);
-        return response()->json(['created' => true, 'data' => $fillable]);
+        return response()->json(['created' => true, 'data'=>$fillable]);
     }
-    public function processWaterBill($request)
+
+       public function api_store(Request $request)
     {
-        if (isset($request['read_date'])) {
-            if (
-                \Carbon\Carbon::parse($request['current_month']) >= $request['read_date'] &&
-                \Carbon\Carbon::parse($request['next_month']) <= $request['read_date']
-            ) {
-                return json_encode(['created' => false, 'msg' => 'Cannot create billing, make sure that the reading date is not covered from the previous reading date.']);
-            }
-        }
+        
+        //no need to have  $request->next_month
+        //request->reading_meter is the value of the meter during the reading (current)
 
-        if ($request['reading_meter'] < $request['meter_reading']) {
-            return json_encode(['created' => false, 'msg' => 'Current meter reading should not be less than the previous meter reading.']);
-        }
+        $this->waterbill->getConnectionType($request->customer_id);
+        $this->waterbill->getCurrentBalance($request->customer_id);
+        $this->waterbill->computeBillConsumption($request->reading_meter);
 
-        $fillable = [
-            'customer_id' => $this->waterbill->balance->customer_id,
-            'period_covered' => $request->current_month . '-' . $request->next_month,
+        $current_month= date('M d, Y ', strtotime($request->current_month));
+        $nextBillDate = date("M d", strtotime($request->current_month . " +1 month"));
+        $current_month_m_y = date("MY", strtotime($request->current_month));
+
+        $dates = explode('-', str_replace([' ', ','], ' ', $this->waterbill->computed_total['date']));
+        $endMonthFromLastBill =  date("M d, Y", strtotime($dates[0]. ', '. date("Y", strtotime($dates[1]))));
+        $endMonthFromLastBill_m_d=date("M d",strtotime($endMonthFromLastBill));
+        $endMonthFromLastBill_m_y=date("MY",strtotime($endMonthFromLastBill));
+ 
+
+$datable = [
+    'request->current_month' => $request->current_month,
+    'nextBillDate' =>$nextBillDate,
+    'current_month_m_y' => $current_month_m_y,
+    'dates' => $dates,
+    'compair' => $endMonthFromLastBill_m_y. " & ".$current_month_m_y,
+     
+    'endMonthFromLastBill' => $endMonthFromLastBill,
+];
+if($this->waterbill->computed_total['meter_consumption'] <0){
+    return response()->json(['created' => false,'message'=>'invalid reading','datable'=>$datable]);
+}
+        if($endMonthFromLastBill_m_y==$current_month_m_y){
+            return response()->json(['created' => false,'message'=>'date-range already exist','datable'=>$datable]);
+        }
+        $fillable=[
+            'customer_id' => $request->customer_id,
+            'period_covered' => $endMonthFromLastBill_m_d.'-'.$current_month, //$nextBillDate
             'reading_date' => date('Y-m-d', strtotime($request->read_date)),
             'reading_meter' => $request->reading_meter,
             'reading_consumption' => $this->waterbill->computed_total['meter_consumption'],
             'billing_amount' => $this->waterbill->computed_total['amount_consumption'],
             'billing_surcharge' => '0.00',
             'billing_meter_ips' => $this->waterbill->balance->billing_meter_ips,
-            'billing_total' => $this->waterbill->computed_total['total'],
-            'balance' => $this->waterbill->computed_total['total'],
+            'billing_total' => number_format($this->waterbill->computed_total['total'],2, '.', ''),
+            'balance' => number_format($this->waterbill->computed_total['total'],2, '.', ''),
             'posted_by' => $request->id,
             'user_id' => $request->id,
         ];
+        
+       $transactions = Transaction::create($fillable);
+        return response()->json(['created' => true, 'data'=>$fillable,'date'=> $this->waterbill->computed_total['date'], 'datable'=>$datable]);
+    }
 
-        $transactions = Transaction::create($fillable);
-        return json_encode(['created' => true, 'data' => $fillable]);
+
+    public function api_store2(): JsonResponse
+    { 
+        
+        // Sample student data
+
+        $students = [
+
+            [
+
+                'id' => 1,
+
+                'name' => 'John Doe',
+
+                'age' => 20,
+
+                'grade' => 'A',
+
+            ],
+
+            [
+
+                'id' => 2,
+
+                'name' => 'Jane Smith',
+
+                'age' => 22,
+
+                'grade' => 'B',
+
+            ],
+
+            // Add more students as needed
+
+        ];
+
+
+
+        // Return JSON response with the sample data
+
+        return response()->json(['students' => $students]);
+
     }
 }
--- app/Http/Controllers/WaterBillController.php
+++ app/Http/Controllers/WaterBillController.php
@@ -1,109 +1,217 @@
 <?php 
 
+
+
 namespace App\Http\Controllers;
 
+
+
 use App\Models\Customer;
+
 use App\Models\Surcharge;
+
 use App\Models\Transaction;
+
 use App\Models\WaterRate;
+
 use Carbon\Carbon;
+
 use Illuminate\Support\Str;
+
 use Illuminate\Http\Request;
+
+
 
 class WaterBillController extends Controller{
 
+
+
     public $surcharge = [];
+
     public $rates = [];
+
     public $connection_type;
+
     public $rate = [];
+
     public $balance = [];
+
     public $computed_total = [];
+
     public $reading_meter;
 
+
+
     public function __construct()
+
     {
+
         $this->rates = WaterRate::all();
+
         $this->surcharge = Surcharge::all();
+
     }
+
+
 
     public function getConnectionType($account_number)
+
     {
+
         $customer = Customer::findOrFail($account_number);
 
+
+
         for($i = 0; $i < count($this->rates); $i++)
+
         {
+
             if(Str::title($customer->connection_type) == $this->rates[$i]->type)
+
             {
+
                 $this->rate = [
+
                     'min_rate' => $this->rates[$i]->min_rate,
+
                     'max_range' => $this->rates[$i]->consumption_max_range,
+
                     'excess_rates' => $this->rates[$i]->excess_rate
+
                 ];
+
             }
+
         }
+
     }
+
+
 
     public function getCurrentBalance($account_number)
+
     {
+
         $bal = Transaction::orderByDesc('created_at')->where('customer_id', $account_number)->get();
+
         $this->balance = $bal->first();
+
     }
+
+
 
     public function computeWaterBill(Request $request)
+
     {
+
         $this->getConnectionType($request->customer_id);
+
         $this->getCurrentBalance($request->customer_id);
+
         $this->reading_meter = $request->reading_meter;
+
         $this->computeBillConsumption($request->reading_meter);
 
+
+
         $surcharge = $this->balance ? ((empty($this->balance->payment_or_no)) ? (($this->balance->balance != null ? $this->balance->balance : 0.00) * $this->surcharge[0]->rate) : 0.00) : 0.00;
+
         $meter_consumption = $request->reading_meter - ($this->balance != null ? $this->balance->reading_meter : 0.00);
+
         $total_consumption = (($meter_consumption - $this->rate['max_range']) * $this->rate['excess_rates']) + $this->rate['min_rate'];
+
         $amount_comsumption = $meter_consumption <= $this->rate['max_range'] ? $this->rate['min_rate'] : $total_consumption;
 
+
+
         $total = ($surcharge + ($this->balance != null ? $this->balance->balance : 0.00)) + (($this->balance != null ? $this->balance->billing_meter_ips : 0.00) + $amount_comsumption);
+
         $date = ($this->balance != null ? ($this->balance->period_covered != "Beginning Balance" ? explode('-', $this->balance->period_covered) : explode('/', '/'.$this->balance->reading_date)) : explode('/', '/'.date('Y-m-d')));
 
+
+
         $this->computed_total = [
+
             'surcharge' => $surcharge,
+
             'meter_consumption' => $meter_consumption,
+
             'total_consumption' => $total_consumption,
+
             'amount_consumption' => $amount_comsumption,
+
             'total' => $total,
+
             'date' => Carbon::parse($date[1])->format('M d').'-'.Carbon::parse($date[1])->addMonths(1)->format('M d, Y')
+
         ];
+
+
 
         return response()->json(['created' => true, 'surcharge' => $this->surcharge, 
+
                                 'rate' => $this->rate, 'bal' => $this->balance,
+
                                 'total' => $this->computed_total]);
+
     }
+
+
 
     public function computeBillConsumption($reading_meter)
+
     {
+
         $surcharge = $this->balance ? ((empty($this->balance->payment_or_no)) ? (($this->balance->balance != null ? $this->balance->balance : 0.00) * $this->surcharge[0]->rate) : 0.00) : 0.00;
+
         $meter_consumption = $reading_meter - ($this->balance != null ? $this->balance->reading_meter : 0.00);
+
         $total_consumption = (($meter_consumption - $this->rate['max_range']) * $this->rate['excess_rates']) + $this->rate['min_rate'];
+
         $amount_comsumption = $meter_consumption <= $this->rate['max_range'] ? $this->rate['min_rate'] : $total_consumption;
 
+
+
         $total = ($surcharge + ($this->balance != null ? $this->balance->balance : 0.00)) + (($this->balance != null ? $this->balance->billing_meter_ips : 0.00) + $amount_comsumption);
+
         $date = ($this->balance != null ? ($this->balance->period_covered != "Beginning Balance" ? explode('-', $this->balance->period_covered) : explode('/', '/'.$this->balance->reading_date)) : explode('/', '/'.date('Y-m-d')));
 
+
+
         $this->computed_total = [
+
             'surcharge' => $surcharge,
+
             'meter_consumption' => $meter_consumption,
+
             'total_consumption' => $total_consumption,
+
             'amount_consumption' => $amount_comsumption,
+
             'total' => $total,
+
             'date' => Carbon::parse($date[1])->format('M d').'-'.Carbon::parse($date[1])->addMonths(1)->format('M d, Y')
+
         ];
+
     }
+
+
 
     public function editAmount($consumption)
+
     {
+
         $amount = ($consumption >= $this->rate['max_range']) ? 
+
                     ((($consumption - $this->rate['max_range']) * ($this->rate['excess_rates'])) + $this->rate['min_rate']) : 
+
                     $this->rate['min_rate'];
 
+
+
         return $amount;
+
     }
+
 }
